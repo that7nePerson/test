8302ad2d8762cbf47a5b535151e719c8235168f1
Kenji Koh <kenjikohzj@gmail.com>
2024-07-06 11:53:24 +0800

    Initial Commit

---
+++ k/kApp.swift
@@ -0,0 +1,17 @@
+//
+//  kApp.swift
+//  k
+//
+//  Created by Kenji Koh on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct kApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
